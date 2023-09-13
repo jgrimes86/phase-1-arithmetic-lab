370ef128b4c3147f2670552a943affc0cee03b6a
Jim Grimes <jimgrimes86@gmail.com>
2023-09-13 09:17:45 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.ceil(Math.random() * 10);
+console.log(random);
+
+let num3 = 39;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(3, 7, 20, 15, 9);
+console.log(max);
